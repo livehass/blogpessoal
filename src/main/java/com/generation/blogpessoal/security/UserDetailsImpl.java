3adb8b566c79196c7b9e3856453b3348a51c1452
livehass <silva.felipe12@hotmail.com>
2024-01-22 20:35:53 -0300

    added methods to user detail impl

--- src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
@@ -1,4 +1,64 @@
 package com.generation.blogpessoal.security;
 
-public class UserDetailsImpl {
+import com.generation.blogpessoal.model.Usuario;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userName;
+    private String passWord;
+
+    private List<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Usuario user) {
+        this.userName = user.getUsuario();
+        this.passWord = user.getSenha();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return passWord;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
