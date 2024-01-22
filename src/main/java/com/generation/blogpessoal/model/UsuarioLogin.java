37b872d9a863dcb3721e631f491a20a15c4b25d2
livehass <silva.felipe12@hotmail.com>
2024-01-22 16:31:55 -0300

    Added new Usuario login model

---
+++ src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
@@ -0,0 +1,18 @@
+package com.generation.blogpessoal.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioLogin {
+
+    private Long id;
+    private String nome;
+    private String usuario;
+    private String senha;
+    private String foto;
+    private String token;
+
+}
