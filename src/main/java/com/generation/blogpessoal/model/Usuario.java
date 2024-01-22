b4f7b591b74c8bd281314aedfbd458bbdb6e90ef
livehass <silva.felipe12@hotmail.com>
2024-01-22 16:32:08 -0300

    Added new Usuario login model

---
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -0,0 +1,44 @@
+package com.generation.blogpessoal.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull(message = "O atributo nome é obrigatório")
+    private String nome;
+
+    @NotNull(message = "O atributo usuuário é obrigatório")
+    @Email(message = "O atributo usuário deve ser um e-mail válido")
+    private String usuario;
+
+    @NotBlank(message = "O atributo senha é obrigatório")
+    @Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres ")
+    private String senha;
+
+    @Size(max=5000, message = "O atributo foto não pode ser maior quuue 5000 caracteres")
+    private String foto;
+
+    @OneToMany(mappedBy = "unuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List<Postagem> postagem;
+
+}
+
+
