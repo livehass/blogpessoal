b286e107cb11aa2b43e0c8f2bb2d9a3fa8ceac84
livehass <silva.felipe12@hotmail.com>
2024-01-22 16:32:34 -0300

    Added new jasonIgnoreCase in postagem model

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,55 +1,44 @@
 package com.generation.blogpessoal.model;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.UpdateTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 
 @Entity
 @Table(name = "tb_postagens")
+@Getter
+@Setter
 public class Postagem {
-	
-	@Getter
-	@Setter
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	@Getter
-	@Setter
+
 	@NotBlank(message = "O Atributo é de preenchimento obrigatório")
 	@Size(min = 5, max = 100, message = "O atributo titulo tem no mínimo 5 caracteres e no máximo 100 caracteres")
 	private String  titulo;
-	
-	@Getter
-	@Setter
+
 	@NotBlank(message = "O Atributo é de preenchimento obrigatório")
 	@Size(min = 5, max = 100, message = "O atributo titulo tem no mínimo 10 caracteres e no máximo 100 caracteres")
 	private String texto;
-	
-	@Getter
-	@Setter
+
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
-	@Getter
-	@Setter
+
 	@ManyToOne
 	@JsonIgnoreProperties("Postagem")
 	private Tema tema;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 }
 
 
